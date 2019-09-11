1cdcc897807ffa9fa13950a79b84f15e56a1b5be
Cracked1 <saifullahdabir@gmail.com>
2019-09-11 12:52:35 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,44 @@
+<?php 
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'xss');
+ 
+/* Attempt to connect to MySQL database */
+$link2 = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ //echo "jhi";
+// Check connection
+if($link2 === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+$sql = "SELECT flag FROM users WHERE username = $username AND password = '$password' ";
+if ($result = $link2->query($sql)) {
+	if ($result->num_rows > 0) {
+						// output data of each row
+		echo '<table>';
+		echo '<tr>';
+		echo '  <td>' . "The Flag is " . '</td>';
+		echo '  </tr> ';           
+		while($row = $result->fetch_assoc())  {
+			echo '<tr>';
+			echo '  <td>' . $row["flag"] . '</td>';
+			echo '  </tr> ';                
+		}
+		echo'</table>';
+	}
+}
+/* else{
+	echo "Error: secret could not be saved";
+} */
+$link1->close();
+
+?>
+
 <html>
    
    <head>
@@ -51,8 +92,136 @@
             </div>
 				
          </div>
-			
+			<br><br><br><?php echo "Login to get the flag!!"; ?>
       </div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+
+Cyber security is a high priority of companies, small and big, as cyber attacks have been on the rise in recent years.
+In response to these attacks, security professionals and college students have been through rigorous training as how hackers are able to get into the companies and how to defend against them.
+One way of cyber security training is through a cyber security capture the flag (CTF) event.
+A cyber security .-.. ...-- - -- ...-- -.-.-- -. CTF is a competition between security professionals and/or students learning about cyber security.
+This competition is used as a learning tool for everyone that is interested in cyber security and it can help sharpen the tools they have learned during their training.The very first cyber security CTF developed and hosted was in 1996 at DEFCON in Las Vegas, Nevada. DEFCON is the largest cyber security conference in the United States and it was officially started in 1993 by Jeff Moss. DEFCON had become a platform for a skills competition and as the Internet grew, both DEFCON and the CTF competitions did as well. CTF competitions have become global as they did not have any borders and can be done via the Internet. International teams were competing for different types of prizes and bragging rights. There are two formats of the cyber security CTF: attack-defend and Jeopardy-style.
+
+
+-->
    </body>
 </html>
